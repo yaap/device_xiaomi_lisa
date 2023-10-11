9c51d742c9d0814744480fa312fb297af468cecc
AmeChanRain <downloadbot007@gmail.com>
2023-10-11 01:08:25 +0000

    lisa: Shim missing symbols for dolbycodec2
    F linker  : CANNOT LINK EXECUTABLE /vendor/bin/hw/dolbycodec2: cannot locate symbol _ZN7android19GraphicBufferSource9configureERKNS_2spINS_16ComponentWrapperEEEiijjj referenced by /vendor/lib/libcodec2_hidl@1.0.stock.so...

--- device.mk
+++ device.mk
@@ -417,6 +417,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Shim for Dolby codec
+PRODUCT_PACKAGES += libshim
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
